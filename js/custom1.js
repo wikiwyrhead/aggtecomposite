1671b14e275d0cbebd90bae972b775d88e32aa48
Arnel Borres Go <arniel.go@gmail.com>
2020-12-21 20:46:00 +0800

    first commit

---
+++ js/custom1.js
@@ -0,0 +1,42 @@
+    function Focus1() {
+        document.getElementById("all1").style.backgroundColor = "darkgreen";
+        document.getElementById("all1").style.color = "white";
+        document.getElementById("all2").style.backgroundColor = "white";
+        document.getElementById("all2").style.color = "black";
+        document.getElementById("all3").style.backgroundColor = "white";
+        document.getElementById("all3").style.color = "black";
+        document.getElementById("all4").style.backgroundColor = "white";
+        document.getElementById("all4").style.color = "black";
+    
+}
+    function Focus2() {
+        document.getElementById("all2").style.backgroundColor = "darkgreen";
+        document.getElementById("all2").style.color = "white";
+        document.getElementById("all1").style.backgroundColor = "white";
+        document.getElementById("all1").style.color = "black";
+        document.getElementById("all3").style.backgroundColor = "white";
+        document.getElementById("all3").style.color = "black";
+        document.getElementById("all4").style.backgroundColor = "white";
+        document.getElementById("all4").style.color = "black";
+}
+    function Focus3() {
+        document.getElementById("all3").style.backgroundColor = "darkgreen";
+        document.getElementById("all3").style.color = "white";
+        document.getElementById("all1").style.backgroundColor = "white";
+        document.getElementById("all1").style.color = "black";
+        document.getElementById("all2").style.backgroundColor = "white";
+        document.getElementById("all2").style.color = "black";
+        document.getElementById("all4").style.backgroundColor = "white";
+        document.getElementById("all4").style.color = "black";
+}
+    function Focus4() {
+        document.getElementById("all4").style.backgroundColor = "darkgreen";
+        document.getElementById("all4").style.color = "white";
+        document.getElementById("all3").style.color = "white";
+        document.getElementById("all1").style.backgroundColor = "white";
+        document.getElementById("all1").style.color = "black";
+        document.getElementById("all2").style.backgroundColor = "white";
+        document.getElementById("all2").style.color = "black";
+        document.getElementById("all3").style.backgroundColor = "white";
+        document.getElementById("all3").style.color = "black";
+}
