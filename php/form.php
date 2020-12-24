e5dfa7c41f152ca6f30f69cd97b737295499c85e
Arnel Borres Go <jjanzen.go@gmail.com>
2020-12-25 00:22:19 +0800

    Working Form.php file (Working Emailing system). (No Official Email Provided yet)

--- php/form.php
+++ php/form.php
@@ -1,17 +1,34 @@
 <?php
+
+//Variable Declaraton
+
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $to = "jjanzen.go@gmail.com";
+    $to_email = "jjanzen.go@gmail.com";
     $subject = "New Quote";
     $phoneNumber = $_POST['phone_number'];
-    $message = $_POST['message'];
-    $headers = "From: $email" . "\r\n" .
-                "CC: somebodyelse@example.com";
-    $send = @mail($to,$subject,$message,$headers);
-    if(@mail($to,$subject,$message,$headers)) {
-        echo "Message was sent";
+    $message = "From: $email\n" . "Client Number: $phoneNumber\n" . "Client Sender: " . $_POST['name'] . "\n\n" . "Message: \n\n" .  $_POST['message'];
+    $headers = 'WPC Cladding & Decking Website <no-reply@aggtrading.com>';
+
+
+//EMAIL CONFIRMATION
+// --- Subject of confirmation email. ---------
+
+$conf_subject = 'Your recent enquiry';
+
+// Who should the confirmation email be from?
+$conf_sender = 'AGGTE WPC Cladding & Decking <no-reply@aggtrading.com>';
+$msg = "Your email: $email\n" . "Your Number: $phoneNumber\n" . "\n" . $_POST['name'] . ",\n\nThank you for your recent enquiry. A member of our
+team will respond to your message as soon as possible.";
+mail( $_POST['email'], $conf_subject, $msg, 'From: ' . $conf_sender );
+
+//----------Send to company email. ---------
+
+    /* $send = @mail($to_email,$subject,$message,$headers); */
+    if(@mail($to_email,$subject,$message,$headers)) {
+        echo "<center><h1>Message was sent</h1><center>";
     }
     else{
-        echo "<h1><marquee>Message not sent.</marquee><h1>";
+        echo "<h1>Message not sent.</h1>";
     }
 ?>
