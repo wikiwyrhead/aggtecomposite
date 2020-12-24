e0853bcbb4e2af66bb8cd3bb7cf1d3eb32cf39a1
Arnel Borres Go <jjanzen.go@gmail.com>
2020-12-24 11:03:26 +0800

    Working Form.Php

--- php/form.php
+++ php/form.php
@@ -1,41 +1,17 @@
 <?php
     $name = $_POST['name'];
-    $visitor_number = $_POST['phonenumber'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
-    $email_from =  "jjanzen.go@gmail.com";
-	$email_subject = "New Quotation";
-	$email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message".
+    $email = $_POST['email'];
     $to = "jjanzen.go@gmail.com";
-    $headers =  'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'From: Your name <info@address.com>' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    mail($to, $email_subject, $email_body, $headers);
-    function IsInjected($str){
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-
-    if(preg_match($inject,$str))
-    {
-      return true;
+    $subject = "New Quote";
+    $phoneNumber = $_POST['phone_number'];
+    $message = $_POST['message'];
+    $headers = "From: $email" . "\r\n" .
+                "CC: somebodyelse@example.com";
+    $send = @mail($to,$subject,$message,$headers);
+    if(@mail($to,$subject,$message,$headers)) {
+        echo "Message was sent";
     }
-    else
-    {
-      return false;
-    }
-}
-
-    if(IsInjected($visitor_email)) {
-    echo "Bad email value!";
-    exit;
+    else{
+        echo "<h1><marquee>Message not sent.</marquee><h1>";
     }
 ?>
