4e4c805a47b5ff2d96eba47ca6f1055a60ab1cab
wiki2 <jjanzen.go@gmail.com>
2021-03-14 13:41:23 +0800

    facebook

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 
           <div class="col-md-5 col-sm-6 col-xs-6">
             <ul class="top-social-icons">
-              <li><a href="https://www.facebook.com/AGG.Trading.WPC.Decking.Cladding" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.facebook.com/AGGTE.Composite.Decking.Cladding" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
             <li><a href="https://twitter.com/share?url=https%3A%2F%2Faquaponicsgogreen.com&text=AquaponicsGoGreen" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
             <li><a href="https://www.instagram.com/aquaponicsgogreenph/"  target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
             <li><a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
