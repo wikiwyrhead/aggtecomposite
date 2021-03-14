76ef8fbb11153e8061a940709b22b51077621bbf
wiki2 <jjanzen.go@gmail.com>
2021-03-14 12:20:36 +0800

    service-single replacement

--- index.php
+++ index.php
@@ -110,7 +110,7 @@
   <div class="container">
     <div class="section-title" href="#ourbestservices">
       <h3>OUR BEST <span>SERVICES</span></h3>
-        <p>Your deck goes beyond the function of <a target="_blank" href="https://en.wikipedia.org/wiki/Wood-plastic_composite#WPC_sandwich_boards">composite boards</a>. It can also be a deck framing or it can also be possibly used as a patio furniture. Every <a target="_blank" href="service-single.php">AGGTE WPC Cladding and Decking</a> piece is made from top grade material, thus, it can withstand weather changes, it is long-lasting and easy to care-for even when installed indoors or outdoors.  On top of these, the perfect blend of elegance and durability that our products have make it stand out in the market and this has greatly contributed to the lasting trust that its clients have been giving to AGGTE.<br><br>
+        <p>Your deck goes beyond the function of <a target="_blank" href="https://en.wikipedia.org/wiki/Wood-plastic_composite#WPC_sandwich_boards">composite boards</a>. It can also be a deck framing or it can also be possibly used as a patio furniture. Every <a target="_blank" href="services.php">AGGTE WPC Cladding and Decking</a> piece is made from top grade material, thus, it can withstand weather changes, it is long-lasting and easy to care-for even when installed indoors or outdoors.  On top of these, the perfect blend of elegance and durability that our products have make it stand out in the market and this has greatly contributed to the lasting trust that its clients have been giving to AGGTE.<br><br>
 Remember, when it comes to your cladding and decking needs, we have it all for you here at AGGTE. So, what are you waiting for, take a look now at our product offering and see it for yourself!
 </p>
     </div>
@@ -122,7 +122,7 @@ Remember, when it comes to your cladding and decking needs, we have it all for y
           <div class="fig_caption">
             <div class="icon"><i class="fa fa-wrench"></i> </div>
             <div class="details">
-              <h3><a href="service-single.php">Wall Cladding</a></h3>
+              <h3><a href="services.php">Wall Cladding</a></h3>
               <p>Exterior composite wall cladding has the capability of making your property look completely refreshed. It is sometimes referred to as house cladding or wall cladding. This material can instantly give a modern make-over on your exteriors.  With this type of cladding material, you can make the facade of your homes and buildings blend into your gardens or make the style of your contemporary domestic or commercial property stand out from the others.
                   <br><br>
                   AGGTE supply high-quality cladding to cover your exterior walls which can improve the kerb appeal of your homes and commercial buildings in an instant.
@@ -138,7 +138,7 @@ Remember, when it comes to your cladding and decking needs, we have it all for y
           <div class="fig_caption">
             <div class="icon"> <i class="fa fa-home"></i> </div>
             <div class="details">
-              <h3><a href="service-single.php">Floor Decking</a></h3>
+              <h3><a href="services.php">Floor Decking</a></h3>
               <p> An excellent composite decking product will provide any outdoor space with a genuine-modern finish, including a wide variety of features that will ensure your protection and peace of mind in the years to come, thus, giving you’re the privilege of enjoying the sight of your deck area.
                   <br><br>
                   We have specified here every detail of our range of product and laid-down their key features. This section of our site will generally give you more information that will give you a concrete idea on the different product features that definitely see AGGTE miles away from its competitors and will lead you to the right direction in choosing for the ideal supplier of your construction needs.
@@ -154,7 +154,7 @@ Remember, when it comes to your cladding and decking needs, we have it all for y
           <div class="fig_caption">
             <div class="icon"><i class="fa fa-users"></i> </div>
             <div class="details">
-              <h3><a href="service-single.php">Builders</a></h3>
+              <h3><a href="services.php">Builders</a></h3>
               <p>AGGTE Composite Builders is an independently owned and operated company. The company and its sales agents or representatives are not in any way connected with AGGTE Composites. As such, AGGTE Composites does not make any representations or warranties as to the products of AGGTE Composite Builders. They are also not responsible for the performance, acts or omissions of AGGTE Composite Builders.<br><br>
            As an independent company, AGGTE Builders have undergone advanced training by joining a very carefully chosen group of installer with extensive and state-of the art experience in the field of WPC Cladding and Decking experience. Through it, AGGTE clients are assured that their concerns can be effectively handled by the very competent members of the company’s management and staff.
             </p>
@@ -167,7 +167,7 @@ Remember, when it comes to your cladding and decking needs, we have it all for y
     <!--row end-->
 
     <div class="readmore text-center">
-            <a href="service-single.php"><button class="main-btn btn-1 btn-1e">All Services</button></a>
+            <a href="services.php"><button class="main-btn btn-1 btn-1e">All Services</button></a>
           </div>
 
   </div>
@@ -391,11 +391,11 @@ Remember, when it comes to your cladding and decking needs, we have it all for y
         <div class="section-title">
           <h3>Our special support<br>
             <span>Why Choose Us </span></h3>
-            <p> We strongly believe that our products are easy to install. As a matter of fact, its assembly does not require highly specialized tools and on top of that, since all the materials that we are using are light-weight, setting it up is also easy. Thus, we can make it possible for you to achieve your desired exterior set-up without consuming much of your time. Here at <a target="_blank" href="service-single.php#decking">AGGTE WPC Cladding and Decking</a>, minimizing waste and ensuring sustainability are our top priorities because our AGGTE calculator can precisely provide you with the exact materials that you will need to complete your project.</p>
+            <p> We strongly believe that our products are easy to install. As a matter of fact, its assembly does not require highly specialized tools and on top of that, since all the materials that we are using are light-weight, setting it up is also easy. Thus, we can make it possible for you to achieve your desired exterior set-up without consuming much of your time. Here at <a target="_blank" href="services.php#decking">AGGTE WPC Cladding and Decking</a>, minimizing waste and ensuring sustainability are our top priorities because our AGGTE calculator can precisely provide you with the exact materials that you will need to complete your project.</p>
             <br>
           <p> Furthermore, our wood composite materials are guaranteed to be more lasting than wood. It has additives that make the existing hard wood properties more sturdy which indicates that you can be assured that your <a target="_blank" href="https://en.wikipedia.org/wiki/Wood-plastic_composite">deck, cladding or fencing</a> is more tough than other ordinary products. Also, our materials do not absorb water like other wooden alternatives so it can endure weather conditions and is also easy to maintain.</p>
             <br>
-            <p> For more information on the featured products on this site, feel free to reach out to any members of our team or you may <a target="_blank" href="service-single.php#decking">download our installation guide.</a></p>
+            <p> For more information on the featured products on this site, feel free to reach out to any members of our team or you may <a target="_blank" href="services.php#decking">download our installation guide.</a></p>
         </div>
         <div class="whychoose-description">
           <ul class="row">
