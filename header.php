cc15bc23ea429990ce9850bfc6e1338ad5252135
wiki2 <jjanzen.go@gmail.com>
2021-03-14 11:37:10 +0800

    Rename services new

--- header.php
+++ header.php
@@ -89,7 +89,7 @@
                     </li>
                     <li class="dropdown"><a class="hvr-link" href="services.php">Services</a>
                       <ul>
-                        <li><a class="hvr-link" href="service-single.php">Services</a></li>
+                        <li><a class="hvr-link" href="services.php">Services</a></li>
                         <li><a class="hvr-link" data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target=".quote-modal" class="brochure-btn theme-btn hvr-link">Get a Quote</a></li>
                           <li><a class="hvr-link" href="cleaning.php">Cleaning Guides</a></li>
                         <li><a class="hvr-link" data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target=".termed">Terms and Conditions</a></li>
@@ -170,9 +170,9 @@
                         <!--<li><a class="hvr-link" href="coming-soon.php">Coming-soon</a></li>-->
                       </ul>
                     </li>
-                    <li class="dropdown"><a class="hvr-link" href="service-single.php">Services</a>
+                    <li class="dropdown"><a class="hvr-link" href="services.php">Services</a>
                       <ul>
-                        <li><a class="hvr-link" href="service-single.php">Services</a></li>
+                        <li><a class="hvr-link" href="services.php">Services</a></li>
                         <li><a class="hvr-link" data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target=".quote-modal" class="brochure-btn theme-btn hvr-link">Get a Quote</a></li>
                           <li><a class="hvr-link" href="cleaning.php">Cleaning Guides</a></li>
                         <li><a class="hvr-link" data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target=".termed">Terms and Conditions</a></li>
