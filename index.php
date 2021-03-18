859110e0881b0978f762f9971de5688968ecfb54
justin <justin.ruiz.go@gmail.com>
2021-03-18 23:14:42 +0800

    Messnger plugin

--- index.php
+++ index.php
@@ -28,9 +28,34 @@
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 <body>
-    <!--Facebook Plugin Start-->
-    <?php include 'fbplug.php'; ?>
-    <!--Facebook Plugin End-->
+
+    <!-- Load Facebook SDK for JavaScript -->
+
+<div id="fb-root"></div>
+<script>
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v10.0'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+  attribution="install_email"
+  page_id="110618234039433">
+</div>
+
+    <!--Load Facebook SDK for JavaScript End Here-->
+
   <div class="page-wrapper">
   <!--preloader start-->
   <div class="preloader"></div>
