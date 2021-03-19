f2280589a5c4f4848a82ffcafb17bd4ff1440ca3
justin <justin.ruiz.go@gmail.com>
2021-03-20 00:53:38 +0800

    quote mailer

--- php/quote.php
+++ php/quote.php
@@ -13,7 +13,7 @@
     $phoneNumber = $_POST['phone_number'];
     $call = $_POST['call'];
     $message = "From: $email\n" . "Client Sender: $name \n" . "Client Number: $phoneNumber\n" . "Call Back?: $call\n" . "\n\n" . "Message: \n\n" .  $_POST['message'];
-    $headers = 'New Quotation Request <no-reply@aggtedeck.com>';
+    $headers = 'New Quotation Request <admin@aggtedeck.com>';
 
 //EMAIL CONFIRMATION
 // --- Subject of confirmation email. ---------
@@ -21,7 +21,7 @@
 $conf_subject = 'Your recent enquiry';
 
 // Who should the confirmation email be from?
-$conf_sender = 'AGGTE WPC Cladding & Decking <no-reply@aggtedeck.com>';
+$conf_sender = 'AGGTE WPC Cladding & Decking <quote@aggtedeck.com>';
 $msg = "Your email: $email\n" . "Your Number: $phoneNumber\n" . "\n" ."Hi" ." ". $_POST['name'] . ",\n\nThank you for your recent enquiry. A member of our team will respond to your message as soon as possible.";
 mail( $_POST['email'], $conf_subject, $msg, 'From: ' . $conf_sender );
 
