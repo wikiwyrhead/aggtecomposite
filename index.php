d36c0b10542ba3f5abc8229b8707a798ecd56ce0
Janzen <jjanzen.go@gmail.com>
2021-03-22 09:23:48 +0800

    repair button

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
       <div class="slidewrap">
         <!--<h3>Welcome to AGGTE WPC Cladding & Decking </h3>-->
         <h1>Transforming your ideas <span>into reality</span></h1>
-        <a href="about.php" class="btn btn-default section-btn">See more</a><a class="btn btn-default section-btn span_color" href="contact">Contact Us</a> </div>
+        <a href="about.php" class="btn btn-default section-btn">See more</a><a class="btn btn-default section-btn span_color" href="contacts.php">Contact Us</a> </div>
     </div>
   </div>
   <!--slider end-->
