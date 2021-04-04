bc3646f60c8d9c4ba28507f6b45c3f0ac8a49365
justin <justin.ruiz.go@gmail.com>
2021-04-05 00:15:29 +0800

    s

--- php/contacts.php
+++ php/contacts.php
@@ -33,7 +33,7 @@ mail( $_POST['email'], $conf_subject, $msg, 'From: ' . $conf_sender );
         echo "<div class='preloader'></div><center><h1 style='position: relative;top: 250px;font-family:Arial;'>Your message was successfully sent!</h1></center>";
         echo "<script>
             window.setTimeout(function() {
-            window.location.href='../contact.php';
+            window.location.href='../contact.php#xs-contact-form';
             }, 2000);
             </script>";
     }
