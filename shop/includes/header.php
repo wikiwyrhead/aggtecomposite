6227f09dba6cec734e842ab4ba51cf2d3f40d529
Janzen <jjanzen.go@gmail.com>
2021-05-15 21:03:10 +0800

    Shop

--- shop/includes/header.php
+++ shop/includes/header.php
@@ -27,6 +27,16 @@
     <link href="css/cart_style.css" rel="stylesheet">
 
   </head>
+  <link href="../css/bootstrap.css" rel="stylesheet">
+<link href="../css/style.css" rel="stylesheet">
+<link href="../css/custom.css" rel="stylesheet">
+<link rel="stylesheet" href="dist/color-default.css">
+<link href="../css/magnific-popup.css" rel="stylesheet">
+<link href="../css/animate.css" rel="stylesheet">
+<link href="../css/owl.css" rel="stylesheet">
+<link href="../css/jquery.fancybox.css" rel="stylesheet">
+<link href="../css/style_slider.css" rel="stylesheet">
+<link href="../css/modal-designs.css" rel="stylesheet">
     <?php include '../header.php'; ?>
 
 
