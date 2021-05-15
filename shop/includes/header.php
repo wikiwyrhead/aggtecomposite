dc03d025e1d2d35268035b350eb7dc6671b6bf9a
Janzen <jjanzen.go@gmail.com>
2021-05-15 19:54:14 +0800

    Shop (not Visible)

--- shop/includes/header.php
+++ shop/includes/header.php
@@ -23,79 +23,12 @@
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin.css" rel="stylesheet">
-
-     <link href="css/cart_style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link href="css/cart_style.css" rel="stylesheet">
 
   </head>
+    <?php include '../header.php'; ?>
 
-  <body id="page-top">
-
-    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-
-      <a class="navbar-brand mr-1" href="index.php">AGGTE SHOP</a>
-
-      <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-        <i class="fas fa-bars"></i>
-      </button>
-
-      <!-- Navbar Search -->
-      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-
-      </form>
-
-      <!-- Navbar -->
-      <ul class="navbar-nav ml-auto ml-md-0">
-        <li class="nav-item dropdown no-arrow mx-1">
-          <!-- <li class="nav-item dropdown no-arrow mx-1">
-          <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-envelope fa-fw"></i>
-            <span class="badge badge-danger">7</span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li> -->
-
-              <?php
-     if (isset($_SESSION['cid'])) {
-      echo '<a class="nav-link " href="admin.php"><i class=" fas fa-user-circle" > '.$_SESSION['C_FNAME'].' '.$_SESSION['C_LNAME'].'</i></a>';
-
-     }else{
-      echo '<a class="nav-link " href="register.php"><i class="fas fa-user-alt ">Sign Up</i></a>';
-     }
-
-      ?>
-
-
-          </a>
-          <!-- <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div> -->
-        </li>
-
-
-            <?php
-     if (isset($_SESSION['cid'])) {
-      echo '<a class="nav-link " href="#" data-toggle="modal" data-target="#logoutModal"><i class=" fas fa-sign-out-alt" >Logout</i></a>';
-     }else{
-      echo '
-          <a class="nav-link " href="login.php" id="userDropdown">
-            <i class=" fas fa-sign-in-alt" > Login</i>
-
-
-          </a>
-
-       ' ;
-
-
-     }
-      ?>
 
 
       </ul>
