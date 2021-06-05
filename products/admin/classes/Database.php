1c6b7c4ce6861f23ae30324683f182a0ae0c01a3
Janzen Go <jjanzen.go@gmail.com>
2021-06-05 16:12:17 +0800

    Products

--- products/admin/classes/Database.php
+++ products/admin/classes/Database.php
@@ -8,7 +8,7 @@ class Database
 	
 	private $con;
 	public function connect(){
-		$this->con = new Mysqli("localhost", "root", "", "ecommerceapp");
+		$this->con = new Mysqli("localhost", "flwbmkwa_root", "0917379784009173797840", "flwbmkwa_ecommerceapp");
 		return $this->con;
 	}
 }
--- products/config/constants.php
+++ products/config/constants.php
@@ -1,11 +1,11 @@
 <?php
 
 define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DATABASE_NAME', 'ecommerceapp');
+define('USER', 'flwbmkwa_root');
+define('PASSWORD', '0917379784009173797840');
+define('DATABASE_NAME', 'flwbmkwa_ecommerceapp');
 
-define('CURRENCY', 'Rs');
+define('CURRENCY', '₱');
 
 
 
