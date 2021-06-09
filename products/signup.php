2433085532ce14abbb38e1020a99f1c4b5f1d418
Janzen <jjanzen.go@gmail.com>
2021-06-09 14:17:16 +0800

    Commit

--- products/signup.php
+++ products/signup.php
@@ -62,7 +62,7 @@
             if(!isset($_SESSION['captcha'])){
               echo '
                 <di class="form-group" style="width:100%;">
-                  <div class="g-recaptcha" data-sitekey="6LevO1IUAAAAAFX5PpmtEoCxwae-I8cCQrbhTfM6"></div>
+                  <div class="g-recaptcha" data-sitekey="6LczUyAbAAAAALImsgGcwBkRt08uIYjAbYWso_z2"></div>
                 </di>
               ';
             }
