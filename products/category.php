11ce17a4ccc44311ace7a9fff6d6ea41eba9c73d
Janzen <jjanzen.go@gmail.com>
2021-06-16 19:37:41 +0800

    Latest Commit

--- products/category.php
+++ products/category.php
@@ -63,7 +63,7 @@
                   $stmt->execute();
                   foreach($stmt as $row){
                     echo "
-                      <li><a href='/?category=".$row['cat_slug']."'>".$row['name']."</a></li>
+                      <li><a href='../?category=".$row['cat_slug']."'>".$row['name']."</a></li>
                     ";                  
                   }
                 }
